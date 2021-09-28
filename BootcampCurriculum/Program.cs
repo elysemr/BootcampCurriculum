1386a82b3103392ccf405341ad30b8aed11bf57b
Elyse Roth <elysemorganroth@gmail.com>
2021-09-28 13:50:17 -0400

    Add project files.

---
+++ BootcampCurriculum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BootcampCurriculum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gitgithub = new Topic();
+            gitgithub.Name = "Git/GitHub";
+            gitgithub.Category = "SCM"; // source control management
+            gitgithub.Difficulty = 3;
+            gitgithub.Print();
+
+            var sql = new Topic();
+            sql.Name = "SQL";
+            sql.Category = "Database Management";
+            sql.Difficulty = 2;
+            sql.Print();
+
+            var csharp = new Topic();
+            csharp.Name = "C#";
+            csharp.Category = "General Purpose Language";
+            csharp.Difficulty = 1;
+            csharp.Print();
+
+            //another way to initialize properties without using constructor
+            var java = new Topic //() optional here
+            {
+                Name = "Java",
+                Category = "General Purpose Language",
+                Difficulty = 1
+
+            };
+            java.Print();
+        }
+    }
+}
---
+++ BootcampCurriculum/Topic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootcampCurriculum
+{
+    class Topic
+    {
+      private static int NextId { get; set; } = 1;
+      public int Id { get; private set; } //can change visibility/access on reading AND editing data
+      public string Name { get; set; }
+      public string Category { get; set; }
+      public int Difficulty { get; set; }
+   
+      public void Print()
+    {
+        Console.WriteLine($"{Id} | {Name} | {Category} | {Difficulty}");
+    }
+
+        public Topic()
+        {
+            this.Id = NextId++;
+           // NextId = NextId++;  did this first, but when incrementing, just need the line above, other increments add this
+        }  
+
+    }
+
+}
